2d99c16a10a9a438e49740e88a278ee46a0cdba3
happy870608 <48850070+happy870608@users.noreply.github.com>
2020-05-10 22:57:04 +0800

    create post1.js

---
+++ post1.js
@@ -0,0 +1,199 @@
+//上傳照片
+// STEP 1: select element and register change event
+const imagePreview = document.querySelector('[data-target="image-preview"]');
+const spinner = document.querySelector('[data-target="spinner"]');
+const fileUploader = document.querySelector('[data-target="file-uploader"]');
+fileUploader.addEventListener("change", handleFileUpload);
+
+async function handleFileUpload(e) {
+  try {
+    const file = e.target.files[0];
+    setUploading(true);
+    if (!file) return;
+
+    const beforeUploadCheck = await beforeUpload(file);
+    if (!beforeUploadCheck.isValid) throw beforeUploadCheck.errorMessages;
+
+    const arrayBuffer = await getArrayBuffer(file);
+    const response = await uploadFileAJAX(arrayBuffer);
+    
+    alert("File Uploaded Success");
+    showPreviewImage(file);
+  } catch (error) {
+    alert(error);
+    console.log("Catch Error: ", error);
+  } finally {
+    e.target.value = '';  // reset input file
+    setUploading(false);
+  }
+}
+
+// STEP 2: showPreviewImage with createObjectURL
+// If you prefer Base64 image, use "FileReader.readAsDataURL"
+function showPreviewImage(fileObj) {
+  const image = URL.createObjectURL(fileObj);
+  imagePreview.src = image;
+}
+
+// STEP 3: change file object into ArrayBuffer
+function getArrayBuffer(fileObj) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    // Get ArrayBuffer when FileReader on load
+    reader.addEventListener("load", () => {
+      resolve(reader.result);
+    });
+
+    // Get Error when FileReader on error
+    reader.addEventListener("error", () => {
+      reject("error occurred in getArrayBuffer");
+    });
+
+    // read the blob object as ArrayBuffer
+    // if you nedd Base64, use reader.readAsDataURL
+    reader.readAsArrayBuffer(fileObj);
+  });
+}
+
+// STEP 4: upload file throguth AJAX
+// - use "new Uint8Array()"" to change ArrayBuffer into TypedArray
+// - TypedArray is not a truely Array,
+//   use "Array.from()" to change it into Array
+function uploadFileAJAX(arrayBuffer) {
+  // correct it to your own API endpoint
+  return fetch("https://jsonplaceholder.typicode.com/posts/", {
+    headers: {
+      version: 1,
+      "content-type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({
+      imageId: 1,
+      icon: Array.from(new Uint8Array(arrayBuffer))
+    })
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw res.statusText;
+      }
+      return res.json();
+    })
+    .then(data => data)
+    .catch(err => console.log("err", err));
+}
+
+// STEP 5: Create before upload checker if needed
+function beforeUpload(fileObject) {
+  return new Promise(resolve => {
+    const validFileTypes = ["image/jpeg", "image/png"];
+    const isValidFileType = validFileTypes.includes(fileObject.type);
+    let errorMessages = [];
+
+    if (!isValidFileType) {
+      errorMessages.push("You can only upload JPG or PNG file!");
+    }
+
+    const isValidFileSize = fileObject.size / 1024 / 1024 < 2;
+    if (!isValidFileSize) {
+      errorMessages.push("Image must smaller than 2MB!");
+    }
+
+    resolve({
+      isValid: isValidFileType && isValidFileSize,
+      errorMessages: errorMessages.join("\n")
+    });
+  });
+}
+
+function setUploading(isUploading) {
+  if (isUploading === true) {
+    spinner.classList.add("opacity-1");
+  } else {
+    spinner.classList.remove("opacity-1");
+  }
+}
+
+//放大圖片
+function imageZoom(imgID, resultID) {
+  var img, lens, result, cx, cy;
+  img = document.getElementById(imgID);
+  result = document.getElementById(resultID);
+  /*create lens:*/
+  lens = document.createElement("DIV");
+  lens.setAttribute("class", "img-zoom-lens");
+  /*insert lens:*/
+  img.parentElement.insertBefore(lens, img);
+  /*calculate the ratio between result DIV and lens:*/
+  cx = result.offsetWidth / lens.offsetWidth;
+  cy = result.offsetHeight / lens.offsetHeight;
+  /*set background properties for the result DIV:*/
+  result.style.backgroundImage = "url('" + img.src + "')";
+  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+  /*execute a function when someone moves the cursor over the image, or the lens:*/
+  lens.addEventListener("mousemove", moveLens);
+  img.addEventListener("mousemove", moveLens);
+  /*and also for touch screens:*/
+  lens.addEventListener("touchmove", moveLens);
+  img.addEventListener("touchmove", moveLens);
+  function moveLens(e) {
+    var pos, x, y;
+    /*prevent any other actions that may occur when moving over the image:*/
+    e.preventDefault();
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    /*calculate the position of the lens:*/
+    x = pos.x - (lens.offsetWidth / 2);
+    y = pos.y - (lens.offsetHeight / 2);
+    /*prevent the lens from being positioned outside the image:*/
+    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
+    if (x < 0) {x = 0;}
+    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+    if (y < 0) {y = 0;}
+    /*set the position of the lens:*/
+    lens.style.left = x + "px";
+    lens.style.top = y + "px";
+    /*display what the lens "sees":*/
+    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+  }
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /*get the x and y positions of the image:*/
+    a = img.getBoundingClientRect();
+    /*calculate the cursor's x and y coordinates, relative to the image:*/
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /*consider any page scrolling:*/
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+
+}
+imageZoom("myimage", "myresult");
+imageZoom("myimage", "myresult1");
+
+//單品敘述
+$(document).ready(function () {
+  $('#ajax-form button[type="submit"]').click((event) => {
+    event.preventDefault()
+    $.post({
+      url: "https://symbolwu-login-web.herokuapp.com/login",
+      dataType: "json",
+      data: {
+        username: $('#ajax-form input[name=fName]').val(),
+       evaluation: $('#ajax-form1 input[name=lName]').val(),
+	   description: $('#ajax-form2 input[name=dName]').val()
+      },
+      success: function (msg) {
+        console.log(msg);
+        alert(msg.text);
+      },
+      //statusCodeç¯„ä¾‹
+      statusCode: {
+        403: function (response) {
+          LocationHerf();
+        }
+      }
+    })
+  })
