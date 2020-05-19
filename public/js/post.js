f97122b5966366a953ad88b26ae7454fe28b66e3
Kazumachi124170 <andmichi1998@example.com>
2020-05-20 00:46:33 +0800

    Fix want and back

--- public/js/post.js
+++ public/js/post.js
@@ -26,29 +26,36 @@ $(document).ready(function() {
       document.getElementById("want_btn_0").innerHTML="已加入通知!"
       document.getElementById("want_number_0").innerHTML=num+1
     }
+
+  });
+
   $('#back').click(function(){
     window.location= "./home.html"
   })
-
-})
-
- $.ajax({
-   url:"https://luffy.ee.ncku.edu.tw:7575/get_post",
-   type:'post',
-   dataType: "json",
-   data: JSON.stringify({
-    id: localStorage.getItem("object_id")
-    }), 
-   contentType: "application/json",
-   success: function(data){
-      console.log(data);
-      $("#title").text(data.post.title);
-      $("#user").text(data.post.name);
-      $("#user_icon").attr("src",data.post.user_icon);
-      $("#post_icon").attr("src",data.post.post_icon);
-      $("#like").val(data.post.like);
-      $("#pen").val(data.post.request);
-      $("#content").text(data.post.explanation)
-   }
- });
+//  $.ajax({
+//    url:"https://luffy.ee.ncku.edu.tw:7575/recommend",
+//    type:'get',
+//    dataType: "json",
+//    contentType: "application/json",
+//    success: function(data){
+//      //console.log(data.object.length)
+//      var html = ""
+//      for(i=0; i<data.object.length; i++){
+//        html = html+ `<div class="posts_post">\
+//            <img class="posts_post_img" src="${data.object[i].post_icon}">\
+//            <div class="posts_post_detail">\
+//              <p class="posts_post_detail_title">Modern room</p>\
+//              <div class="posts_post_detail_account">\
+//                <p class="posts_post_detail_account_id">eric_50336</p>\
+//                <img class="posts_post_detail_account_img" src="./res/img/request_1.png">\
+//              </div>\
+//            </div>\
+//          </div>`
+//      }
+//        $('#posts').html(html)
+//        // data.object.forEach(element => console.log(element));
+//        // $("#post_img").attr("src",data.object[0].post_icon)
+//        // $("#user_img").attr("src",data.object[0].user_icon)
+//    }
+//  });
 });
