e52352e7ae2a217d6c10fd4ff14d62cfc495feec
yuchi <g870627@gmail.com>
2020-05-19 21:12:43 +0800

    modal test

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,29 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("signup_button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 /*function check(){
   var forms = document.getElementById('register_form');
   if(forms.checkValidity()){
@@ -98,3 +124,5 @@ $(document).ready(function () {
 var LocationHerf = function () {
   window.location.href = "https://luffy.ee.ncku.edu.tw:7575/html/login.html#Back";
 };
+
+
