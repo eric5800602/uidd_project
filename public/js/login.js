822142969beb0d210c6a02ca6a6de9a3c377fcbf
yuchi <g870627@gmail.com>
2020-05-21 00:21:59 +0800

    signup2 ajax

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 var sign_password, sign_username, sign_email;
-function check(){
+/*function check(){
   var forms = document.getElementById('register_form');
   if(forms.checkValidity()){
     console.log($('#register_form input[name=name]').val(),$('#register_form input[name=email]').val(),$('#register_form input[name=phone]').val(),$('#register_form select[name=gender]').val(),$('#register_form input[name=password]').val());
@@ -38,12 +38,11 @@ function completeAndRedirect(){
       console.log(data);
     }
   })
-}
+}*/
 $(document).ready(function () {
   
-
   $('#ajax_login_signup button[id="login"]').click((event) => {
-    event.preventDefault()
+    event.preventDefault();
     $.post({
       url: "https://luffy.ee.ncku.edu.tw:7575/login",
       dataType: "json",
@@ -76,7 +75,7 @@ $(document).ready(function () {
   $('#ajax_login_signup button[id="signup"]').click((event) => {
     event.preventDefault();
   })
-  
+  /*
   $('#register_form button[id="signup"]').click((event) => {
     console.log(check());
     console.log($('#register_form input[name=name]').val(),$('#register_form input[name=email]').val(),$('#register_form input[name=phone]').val(),$('#register_form select[name=gender]').val(),$('#register_form input[name=password]').val());
@@ -99,16 +98,99 @@ $(document).ready(function () {
       }
     })
   })
+  */
 
-
-  /* close html defalt setting*/
+  /* Modal1 Next button*/
   $('#modal1_button button[id="Next_button"]').click((event) => {
-    event.preventDefault();
+    event.preventDefault(); /* close html defalt setting*/
     window.sign_password = $('#modal1_input input[name=Password]').val();
     window.sign_username = $('#modal1_input input[name=Username]').val();
     window.sign_email = $('#modal1_input input[name=Email]').val();
   })
-
+    
+  /* Modal2 Next button*/
+  $('#modal2_button button[id="Submit_button"]').click((event) => {
+    var check = false;
+    var suite = false;
+    if(document.getElementById('switcher-1').checked = true){
+      check = false;
+    }
+    else{
+      check = true;
+    }
+    if(document.getElementById('suite').clicked == true){
+      suite = true;
+    }
+    $.post({
+      url: "https://luffy.ee.ncku.edu.tw:7575/register",
+      dataType: "json",
+      contentType: "application/json",
+      xhrFields: {
+        withCredentials: true
+      },
+      data: {
+        username: window.sign_username,
+        password: window.sign_password ,
+        email: window.sign_email,
+        designer: check,
+        room: $('#modal2_input select[name=modal2_room]').val(),
+        hall: $('#modal2_input input[name=modal2_hall]').val(),
+        bath: $('#modal2_input input[name=modal2_bath]').val(),
+        suite: suite
+      },
+      success: function (data) {
+        alert(data.text);
+        /*
+        $('#SignModal2').modal('hide');
+        $('#register_form input[name=Username]').val("")
+        $('#register_form input[name=Email]').val("")
+        $('#register_form input[name=phone]').val("")
+        $('#register_form select[name=gender]').val("")
+        $('#register_form input[name=password]').val("")
+        $('#register_form input[name=password_confirmation]').val("")*/
+        window.location.href = "https://luffy.ee.ncku.edu.tw:7575/html/tags.html";
+      },
+      error: function(data){
+        console.log(data);
+      }
+    })
+  })
+  /* Modal2 Next button*/
+  $('#modal2_button span[id="skip_button"]').click((event) => {
+    $.post({
+      url: "https://luffy.ee.ncku.edu.tw:7575/register",
+      dataType: "json",
+      contentType: "application/json",
+      xhrFields: {
+        withCredentials: true
+      },
+      data: {
+        username: window.sign_username,
+        password: window.sign_password ,
+        email: window.sign_email,
+        designer: undefined,
+        room: undefined,
+        hall: undefined,
+        bath: undefined,
+        suite: undefined
+      },
+      success: function (data) {
+        alert(data.text);
+        /*
+        $('#SignModal2').modal('hide');
+        $('#register_form input[name=Username]').val("")
+        $('#register_form input[name=Email]').val("")
+        $('#register_form input[name=phone]').val("")
+        $('#register_form select[name=gender]').val("")
+        $('#register_form input[name=password]').val("")
+        $('#register_form input[name=password_confirmation]').val("")*/
+        window.location.href = "https://luffy.ee.ncku.edu.tw:7575/html/tags.html";
+      },
+      error: function(data){
+        console.log(data);
+      }
+    })
+  })
   $('#forgetpasswordbutton').click((event) => {
     console.log();
   });
