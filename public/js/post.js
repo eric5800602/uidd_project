28f289b31bd822e67a22a1a8efc8a928a302698d
eric5800602 <eric5800602@gmail.com>
2020-05-20 20:44:56 +0800

    [PAGES] Resume the home.js

--- public/js/post.js
+++ public/js/post.js
@@ -32,30 +32,25 @@ $(document).ready(function() {
   $('#back').click(function(){
     window.location= "./home.html"
   })
-//  $.ajax({
-//    url:"https://luffy.ee.ncku.edu.tw:7575/recommend",
-//    type:'get',
-//    dataType: "json",
-//    contentType: "application/json",
-//    success: function(data){
-//      //console.log(data.object.length)
-//      var html = ""
-//      for(i=0; i<data.object.length; i++){
-//        html = html+ `<div class="posts_post">\
-//            <img class="posts_post_img" src="${data.object[i].post_icon}">\
-//            <div class="posts_post_detail">\
-//              <p class="posts_post_detail_title">Modern room</p>\
-//              <div class="posts_post_detail_account">\
-//                <p class="posts_post_detail_account_id">eric_50336</p>\
-//                <img class="posts_post_detail_account_img" src="./res/img/request_1.png">\
-//              </div>\
-//            </div>\
-//          </div>`
-//      }
-//        $('#posts').html(html)
-//        // data.object.forEach(element => console.log(element));
-//        // $("#post_img").attr("src",data.object[0].post_icon)
-//        // $("#user_img").attr("src",data.object[0].user_icon)
-//    }
-//  });
+ $.ajax({
+   url:"/get_post",
+   type:'post',
+   dataType: "json",
+   data:{
+     id:localStorage.getItem("post_id")
+   },
+   contentType: "application/json",
+   success: function(data){
+     $('#post_title').text(data.post.title);
+     $('#post_id').text(data.post.name);
+     $('#user_icon').attr("src",data.post.user_icon);
+     $('#post_icon').attr("src",data.post.post_icon);
+     $('#like_count').text(data.post.like);
+     $('#pen_count').text(data.post.request);
+     $('#post_content').text(data.post.explanation);
+     $('#post_id').text(data.post.name);
+       // $("#post_img").attr("src",data.object[0].post_icon)
+       // $("#user_img").attr("src",data.object[0].user_icon)
+   }
+ });
 });
