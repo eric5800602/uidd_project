28865bc9c205dfc1730b70c5d235df9cc73140a1
happy870608 <48850070+happy870608@users.noreply.github.com>
2020-06-24 01:04:32 +0800

    Update testpost.js

--- public/js/testpost.js
+++ public/js/testpost.js
@@ -17,9 +17,6 @@ function cameraStart() {
         console.error("Oops. Something is broken.", error);
     });
 };
-
-//back and next
-
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
@@ -46,6 +43,7 @@ cameraTrigger.onclick = function() {
             setTimeout(function () {
             $("#tag").css('opacity','1');
             }, 1000);
+			
             cameraView.srcObject.getTracks().forEach(function(track) {
                 track.stop();
               });
@@ -88,8 +86,8 @@ $('#file').change(function() {
             $("#posting").css('opacity','1');
             }, 1000);
             setTimeout(function () {
-            $("#tag").css('opacity','1');
-            }, 1000);
+            $("#Submit_button").css('opacity','1');
+            }, 100);
             cameraView.srcObject.getTracks().forEach(function(track) {
                 track.stop();
               });
@@ -104,23 +102,6 @@ $('#file').change(function() {
 file.onclick = function() {
 	
 };
- var tags = document.getElementsByClassName('tags')
-    for(var i = 0; i < tags.length; i++) {
-        (function(index) {
-            console.log(i);
-            tags[index].addEventListener("click", function() {
-                if (this.classList.contains("tags_click")) {
-                    this.classList.add("tags_double_click");
-                    this.classList.remove("tags_click");
-                } else {
-                    this.classList.add("tags_click");
-                    if (this.classList.contains("tags_double_click")) {
-                        this.classList.remove("tags_double_click");
-                    }
-                }
-           })
-        })(i);
-      }
 //select
 var sel1 = document.querySelector('#sel1');
 var sel2 = document.querySelector('#sel2');
@@ -140,9 +121,9 @@ var myTargetValue = myTarget.value;
 $( "#sel1" ).change(function() {
 	if($("#sel1").val()=="single"){
 		 $("#pings").css('opacity','0');
-		 $("#next").css('opacity','0');
-		  $("#Submit_button").css('z-index','1');
-		$("#Submit_button").css('opacity','0');
+		  $("#next").css('opacity','0');
+		  $("#Submit_button").css('opacity','0');
+		  $("#Submit_button").css('z-index','0');
 		  $("#Submit_button1").css('opacity','1');
 		  $("#Submit_button1").css('z-index','3');
 		  
@@ -152,14 +133,30 @@ $( "#sel1" ).change(function() {
 		  $("#next").css('opacity','1');
 		   $("#Submit_button").css('opacity','1');
 		   $("#Submit_button").css('z-index','3');
-	      $("#Submit_button1").css('opacity','0');
-		   $("#Submit_button1").css('z-index','1');
-	
+		   $("#Submit_button1").css('opacity','0');
+		   $("#Submit_button1").css('z-index','0');
 	}
 });
 
+//tags
+ var tags = document.getElementsByClassName('tags')
+    for(var i = 0; i < tags.length; i++) {
+        (function(index) {
+            console.log(i);
+            tags[index].addEventListener("click", function() {
+                if (this.classList.contains("tags_click")) {
+                    this.classList.add("tags_double_click");
+                    this.classList.remove("tags_click");
+                } else {
+                    this.classList.add("tags_click");
+                    if (this.classList.contains("tags_double_click")) {
+                        this.classList.remove("tags_double_click");
+                    }
+                }
+           })
+        })(i);
+      }
 	  var confirm = document.getElementById('Submit_button');
-
     confirm.addEventListener("click",function(){
         var tags = new Array();
         var tmp = document.getElementsByClassName('tags_click');
@@ -186,22 +183,51 @@ $( "#sel1" ).change(function() {
         explanation: $('#explanation').val(),
 		tags: $('#ajax-form3 input[name=tName]').val(),
 		tags:tags,
-			
       }), 
-	  
+	 
+      success: function (msg) {
+        console.log(msg);
+        if(msg.success){
+          console.log("good");
+         
+        }
+        else{
+          console.log("fail");
+          //window.location.href = "https://luffy.ee.ncku.edu.tw:7575/html/home.html";
+          alert(msg.text);
+        }
+      },
+      error: function(data){
+        console.log("fail");
+        console.log(data);
+      }
+    })
+  })
+	 })
+	  $(document).ready(function () {
+		 $('#Submit_button1').click((event) => {
+    $.post({
+      url: "https://luffy.ee.ncku.edu.tw:7575/add_post",
+      dataType: "json",
+      contentType: "application/json",
+      xhrFields: {
+        withCredentials: true
+      },
+		data: JSON.stringify({
+		space: $("#sel1").val(),
+		room:$("#sel2").val(),
+		pings: $('#ajax-form4 input[name=pings]').val(),
+        title: $('#ajax-form input[name=fName]').val(),
+        explanation: $('#explanation').val(),
+		tags: $('#ajax-form3 input[name=tName]').val(),
+		tags:tags,
+      }), 
+	 
       success: function (msg) {
         console.log(msg);
         if(msg.success){
           console.log("success");
-          if($("#sel1").val()=="space"){
-		    $("#Submit_button1").css('display','none');
-           window.location= "./post1.html" 
-		  }
-		  else{
-			    $("#Submit_button").css('display','none');
-			  window.location= "./home.html" 
-        }
-
+         
         }
         else{
           console.log("fail");
@@ -217,46 +243,4 @@ $( "#sel1" ).change(function() {
   })
 	 })
 	 })
-/*
-//上傳照片
-        var file = $("#file")[0];
-        //這裡使用的是onchange事件，所以當你選擇完檔案之後，就觸發事件上傳
-        file.onchange = function () {
-            //建立一個FormDate
-            var formData = new FormData();
-            //將檔案資訊追加到其中
-            formData.append('file', file.files[0]);
-            //利用split切割，拿到上傳檔案的格式
-            var src = file.files[0].name,
-                formart = src.split(".")[1];
-            //使用if判斷上傳檔案格式是否符合                                                          
-            if (formart == "jpg" || formart == "png" || formart == "PNG" ||
-                formart == "docx" || formart == "txt" ||
-                formart == "ppt" || formart == "xlsx" ||
-                formart == "zip" || formart == "rar" ||
-                formart == "doc") {
-                //只有滿足以上格式時，才會觸發ajax請求
-                $.ajax({
-                    url: '/upload',
-                    type: 'POST',
-                    data: formData,
-                    cache: false,
-                    contentType: false,
-                    processData: false,
-                    success: function (data) {
-                        //上傳成功之後，返回物件data         
-                        if (data.success===true) {
-                            var src = data.data;
-                            console.log(formart)
-                            if (formart == "png" || formart == "jpg") {
-                                $('#img').attr('src',src)
-                            }
-                            console.log(`Url of img: ${data.data}`)
-                            // 這裡將msg 追加到你要顯示的區域 
-                            localStorage.setItem("url", data.data)
-                        }
-                    }})
-                    //不滿足上傳格式時 
-            }
-            console.log(sessionStorage.getItem("url"));
-        }*/
+
