14ba9d506335e824457d1a03b855f8c80ec0a9c5
happy870608 <48850070+happy870608@users.noreply.github.com>
2020-06-28 03:05:35 +0800

    Update post1.js

--- public/js/post1.js
+++ public/js/post1.js
@@ -4,16 +4,13 @@ const imagePreview = document.querySelector('[data-target="image-preview"]');
 const spinner = document.querySelector('[data-target="spinner"]');
 const fileUploader = document.querySelector('[data-target="file-uploader"]');
 fileUploader.addEventListener("change", handleFileUpload);
-
 async function handleFileUpload(e) {
   try {
     const file = e.target.files[0];
     setUploading(true);
     if (!file) return;
-
     const beforeUploadCheck = await beforeUpload(file);
     if (!beforeUploadCheck.isValid) throw beforeUploadCheck.errorMessages;
-
     const arrayBuffer = await getArrayBuffer(file);
     const response = await uploadFileAJAX(arrayBuffer);
     
@@ -27,14 +24,12 @@ async function handleFileUpload(e) {
     setUploading(false);
   }
 }
-
 // STEP 2: showPreviewImage with createObjectURL
 // If you prefer Base64 image, use "FileReader.readAsDataURL"
 function showPreviewImage(fileObj) {
   const image = URL.createObjectURL(fileObj);
   imagePreview.src = image;
 }
-
 // STEP 3: change file object into ArrayBuffer
 function getArrayBuffer(fileObj) {
   return new Promise((resolve, reject) => {
@@ -43,18 +38,15 @@ function getArrayBuffer(fileObj) {
     reader.addEventListener("load", () => {
       resolve(reader.result);
     });
-
     // Get Error when FileReader on error
     reader.addEventListener("error", () => {
       reject("error occurred in getArrayBuffer");
     });
-
     // read the blob object as ArrayBuffer
     // if you nedd Base64, use reader.readAsDataURL
     reader.readAsArrayBuffer(fileObj);
   });
 }
-
 // STEP 4: upload file throguth AJAX
 // - use "new Uint8Array()"" to change ArrayBuffer into TypedArray
 // - TypedArray is not a truely Array,
@@ -81,30 +73,25 @@ function uploadFileAJAX(arrayBuffer) {
     .then(data => data)
     .catch(err => console.log("err", err));
 }
-
 // STEP 5: Create before upload checker if needed
 function beforeUpload(fileObject) {
   return new Promise(resolve => {
     const validFileTypes = ["image/jpeg", "image/png"];
     const isValidFileType = validFileTypes.includes(fileObject.type);
     let errorMessages = [];
-
     if (!isValidFileType) {
       errorMessages.push("You can only upload JPG or PNG file!");
     }
-
     const isValidFileSize = fileObject.size / 1024 / 1024 < 2;
     if (!isValidFileSize) {
       errorMessages.push("Image must smaller than 2MB!");
     }
-
     resolve({
       isValid: isValidFileType && isValidFileSize,
       errorMessages: errorMessages.join("\n")
     });
   });
 }
-
 function setUploading(isUploading) {
   if (isUploading === true) {
     spinner.classList.add("opacity-1");
@@ -112,7 +99,6 @@ function setUploading(isUploading) {
     spinner.classList.remove("opacity-1");
   }
 }
-
 //放大圖片
 function imageZoom(imgID, resultID) {
   var img, lens, result, cx, cy;
@@ -173,32 +159,55 @@ function imageZoom(imgID, resultID) {
 imageZoom("myimage", "myresult");
 
 
+
 //單品敘述
 $(document).ready(function () {
-  $('#ajax-form button[type="submit"]').click((event) => {
-    event.preventDefault()
-    $.post({
-      url: "http://luffy.ee.ncku.edu.tw:7575/add_post",
+@@ -181,7 +182,7 @@ $(document).ready(function () {
       dataType: "json",
       data: JSON.stringify({
         username: $('#ajax-form input[name=fName]').val(),
+       evaluation: $('#ajax-form1 input[name=lName]').val(),
        evaluation: $('#evaluation').val(),
 	   description: $('#ajax-form2 input[name=dName]').val()
       }), 
       contentType: "application/json",
-      crossDomain: true,
-      xhrFields: {
-        withCredentials: true
-      },
-      "success": true,
-       text: "Post success, test"
-      },
+@@ -195,39 +196,9 @@ $(document).ready(function () {
     )
   })
  });
- 
- 
- 
- //評價
-
-  
+$('.submit_button').click(function(){
+    window.location= "./post.html" 
+  });
+/* 處理圖片回傳切好的50*50大小圖片*/
+document.getElementById("myimage").addEventListener('click',function(e){
+  var x = event.pageX - (this.getBoundingClientRect().x+parseInt((window.getComputedStyle(this, null).getPropertyValue('padding-left'))));
+  var y = event.pageY - (this.getBoundingClientRect().y + parseInt((window.getComputedStyle(this, null).getPropertyValue('padding-top'))));
+  if(x < 0) x = 0;
+  if(y < 0) y = 0;
+  console.log(x,y,this.getAttribute("src"));
+  $.ajax({
+    url: '/cropimage',
+    type: 'POST',
+    dataType: "json",
+    contentType: "application/json",
+    xhrFields: {
+    withCredentials: true
+    },
+    data: JSON.stringify({
+        url:this.getAttribute("src"),
+        x:parseInt(x),
+        y:parseInt(y),
+        width:this.width,
+        height:this.height
+    }),
+    success: function (data) {
+        //上傳成功之後，返回物件data         
+        console.log(data)
+        if(data.success === false){
+            alert("Crop image fail");
+        }
+        else if(data.success === true){
+            document.getElementById("target1").src = data.url;
+        }
+  }})
+}) 
