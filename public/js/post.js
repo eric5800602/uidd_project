63e2eaad03108e763213c30ea9c20884dfaa1250
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-28 18:35:03 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -32,6 +32,7 @@ $('.photo').click(function(){
 
 $('.div_source').click(function(){
   source=!source;
+  console.log("source= "+source)
   if(source==true){
     $('.div_source').animate({"background-color": '#8C8C8C4D'});
   }else{
