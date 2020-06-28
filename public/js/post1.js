79355295b2fc8afdef9f810849a519b2fbefc70f
happy870608 <48850070+happy870608@users.noreply.github.com>
2020-06-28 15:55:47 +0800

    Update post1.js

--- public/js/post1.js
+++ public/js/post1.js
@@ -1,155 +1,55 @@
-//上傳照片
-// STEP 1: select element and register change event
-const imagePreview = document.querySelector('[data-target="image-preview"]');
-const spinner = document.querySelector('[data-target="spinner"]');
-const fileUploader = document.querySelector('[data-target="file-uploader"]');
-fileUploader.addEventListener("change", handleFileUpload);
-async function handleFileUpload(e) {
-  try {
-    const file = e.target.files[0];
-    setUploading(true);
-    if (!file) return;
-    const beforeUploadCheck = await beforeUpload(file);
-    if (!beforeUploadCheck.isValid) throw beforeUploadCheck.errorMessages;
-    const arrayBuffer = await getArrayBuffer(file);
-    const response = await uploadFileAJAX(arrayBuffer);
-    
-    alert("File Uploaded Success");
-    showPreviewImage(file);
-  } catch (error) {
-    alert(error);
-    console.log("Catch Error: ", error);
-  } finally {
-    e.target.value = '';  // reset input file
-    setUploading(false);
-  }
-}
-// STEP 2: showPreviewImage with createObjectURL
-// If you prefer Base64 image, use "FileReader.readAsDataURL"
-function showPreviewImage(fileObj) {
-  const image = URL.createObjectURL(fileObj);
-  imagePreview.src = image;
-}
-// STEP 3: change file object into ArrayBuffer
-function getArrayBuffer(fileObj) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    // Get ArrayBuffer when FileReader on load
-    reader.addEventListener("load", () => {
-      resolve(reader.result);
-    });
-    // Get Error when FileReader on error
-    reader.addEventListener("error", () => {
-      reject("error occurred in getArrayBuffer");
-    });
-    // read the blob object as ArrayBuffer
-    // if you nedd Base64, use reader.readAsDataURL
-    reader.readAsArrayBuffer(fileObj);
-  });
-}
-// STEP 4: upload file throguth AJAX
-// - use "new Uint8Array()"" to change ArrayBuffer into TypedArray
-// - TypedArray is not a truely Array,
-//   use "Array.from()" to change it into Array
-function uploadFileAJAX(arrayBuffer) {
-  // correct it to your own API endpoint
-  return fetch("https://jsonplaceholder.typicode.com/posts/", {
-    headers: {
-      version: 1,
-      "content-type": "application/json"
-    },
-    method: "POST",
-    body: JSON.stringify({
-      imageId: 1,
-      icon: Array.from(new Uint8Array(arrayBuffer))
-    })
-  })
-    .then(res => {
-      if (!res.ok) {
-        throw res.statusText;
-      }
-      return res.json();
-    })
-    .then(data => data)
-    .catch(err => console.log("err", err));
-}
-// STEP 5: Create before upload checker if needed
-function beforeUpload(fileObject) {
-  return new Promise(resolve => {
-    const validFileTypes = ["image/jpeg", "image/png"];
-    const isValidFileType = validFileTypes.includes(fileObject.type);
-    let errorMessages = [];
-    if (!isValidFileType) {
-      errorMessages.push("You can only upload JPG or PNG file!");
-    }
-    const isValidFileSize = fileObject.size / 1024 / 1024 < 2;
-    if (!isValidFileSize) {
-      errorMessages.push("Image must smaller than 2MB!");
-    }
-    resolve({
-      isValid: isValidFileType && isValidFileSize,
-      errorMessages: errorMessages.join("\n")
-    });
-  });
-}
-function setUploading(isUploading) {
-  if (isUploading === true) {
-    spinner.classList.add("opacity-1");
-  } else {
-    spinner.classList.remove("opacity-1");
-  }
-}
+
 //放大圖片
-function imageZoom(imgID, resultID) {
+/*function imageZoom(imgID, resultID) {
   var img, lens, result, cx, cy;
   img = document.getElementById(imgID);
   result = document.getElementById(resultID);
-  /*create lens:*/
+  //create lens:
   lens = document.createElement("DIV");
   lens.setAttribute("class", "img-zoom-lens");
-  /*insert lens:*/
+  //insert lens:
   img.parentElement.insertBefore(lens, img);
-  /*calculate the ratio between result DIV and lens:*/
+  //calculate the ratio between result DIV and lens:
   cx = result.offsetWidth / lens.offsetWidth;
   cy = result.offsetHeight / lens.offsetHeight;
-  /*set background properties for the result DIV:*/
+  //set background properties for the result DIV:
   result.style.backgroundImage = "url('" + img.src + "')";
   result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-  /*execute a function when someone moves the cursor over the image, or the lens:*/
+  //execute a function when someone moves the cursor over the image, or the lens:
   lens.addEventListener("mousemove", moveLens);
-  img.addEventListener("mousemove", moveLens);
-  /*and also for touch screens:*/
+  img.addEventListener("mousemove", moveLens)
+  //and also for touch screens:
   lens.addEventListener("touchmove", moveLens);
   img.addEventListener("touchmove", moveLens);
   function moveLens(e) {
     var pos, x, y;
-    /*prevent any other actions that may occur when moving over the image:*/
+    //prevent any other actions that may occur when moving over the image:
     e.preventDefault();
-    /*get the cursor's x and y positions:*/
+    //get the cursor's x and y positions:
     pos = getCursorPos(e);
-    /*calculate the position of the lens:*/
+    //calculate the position of the lens:
     x = pos.x - (lens.offsetWidth / 2);
     y = pos.y - (lens.offsetHeight / 2);
-    /*prevent the lens from being positioned outside the image:*/
+    //prevent the lens from being positioned outside the image:
     if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
     if (x < 0) {x = 0;}
     if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
     if (y < 0) {y = 0;}
-    /*set the position of the lens:*/
+    //set the position of the lens:
     lens.style.left = x + "px";
     lens.style.top = y + "px";
-    /*display what the lens "sees":*/
+    //display what the lens "sees":
     result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
   }
   function getCursorPos(e) {
     var a, x = 0, y = 0;
     e = e || window.event;
-    /*get the x and y positions of the image:*/
+    //get the x and y positions of the image:
     a = img.getBoundingClientRect();
-    /*calculate the cursor's x and y coordinates, relative to the image:*/
+    //calculate the cursor's x and y coordinates, relative to the image:
     x = e.pageX - a.left;
     y = e.pageY - a.top;
-    /*consider any page scrolling:*/
+    //consider any page scrolling:
     x = x - window.pageXOffset;
     y = y - window.pageYOffset;
     return {x : x, y : y};
@@ -157,57 +57,113 @@ function imageZoom(imgID, resultID) {
 
 }
 imageZoom("myimage", "myresult");
-
-
+*/
 
 //單品敘述
 $(document).ready(function () {
-$(document).ready(function () {
+ /* $('#ajax-form button[type="submit"]').click((event) => {
+    event.preventDefault()
+    $.post({
+      url: "http://luffy.ee.ncku.edu.tw:7575/add_post",
       dataType: "json",
       data: JSON.stringify({
         username: $('#ajax-form input[name=fName]').val(),
-       evaluation: $('#ajax-form1 input[name=lName]').val(),
        evaluation: $('#evaluation').val(),
 	   description: $('#ajax-form2 input[name=dName]').val()
       }), 
       contentType: "application/json",
- $(document).ready(function () {
+      crossDomain: true,
+      xhrFields: {
+        withCredentials: true
+      },
+      success: function (msg) {
+        console.log(msg);
+        if(msg.success){
+			 console.log("success");
+	 }
+		 
+        }
+        else{
+          console.log("fail");
+          //window.location.href = "https://luffy.ee.ncku.edu.tw:7575/html/home.html";
+          alert(msg.text);
+        }
+      },
+      error: function(data){
+        console.log("fail");
+        console.log(data);
+      }
+      },
     )
   })
- });
-$('.submit_button').click(function(){
-    window.location= "./post.html" 
+ });*/
+ 
+ /*$('#myimage').click.function(){
+	 $('.img-zoom-result').css('backgroundImage' , 'url("'../res/post1.png'")');
+ })*/
+ 
+ //放大圖片
+ /*
+ $(document).on("id_content",function(){
+  $("#myimage").on("swipe",function(){
+    $(".img-zoom-result").css(
+  });                       
+});
+*/
+window.is_myimage_touching = false;
+var MYIMAGE_SELECTOR = '#myimage', $myimage = $(MYIMAGE_SELECTOR), myimageOffset = $myimage.offset();
+$(document).on('touchstart touchend', MYIMAGE_SELECTOR, function(e){
+	window.is_myimage_touching = e.type==='touchstart';
+});
+//$('.img-zoom-result').css('background-image',$myimage.attr('src'));
+window.is_handle_myimage_touching=false;
+var $imgzoomresult = $('.img-zoom-result');
+$(document).on('touchmove',MYIMAGE_SELECTOR,function(e){
+	//if(!window.is_myimage_touching){return;}
+	//if(window.is_handle_myimage_touching){return;}
+	window.is_handle_myimage_touching=true;
+	console.log(e.touches[0].clientX, e.touches[0].clientY, myimageOffset.top, myimageOffset.left);
+	var tmp = e.touches[0];
+	var x = tmp.clientX - 100; ///window.innerWidth; // - myimageOffset.left + 0*$imgzoomresult.width()/2;
+	var y = (tmp.clientY-myimageOffset.top - 100 - window.innerHeight*0.04); // *(window.innerWidth/375); // - myimageOffset.top + 0*$imgzoomresult.height()/2;
+	console.log(x,y);
+	$imgzoomresult.find('img').css({top:-y, left:-x}).attr('src',$myimage.attr('src'));
+	window.is_handle_myimage_touching=false;
+});
+
+
+ 		 $('#addsingle').click((event) => {
+			 
+    $.post({
+      url: "https://luffy.ee.ncku.edu.tw:7575/add_single",
+      dataType: "json",
+      contentType: "application/json",
+      xhrFields: {
+        withCredentials: true
+      },
+		data: JSON.stringify({
+		username: $('#ajax-form input[name=fName]').val(),
+       evaluation: $('#evaluation').val(),
+	   description: $('#ajax-form2 input[name=dName]').val()
+      }), 
+      success: function (msg) {
+        console.log(msg);
+        if(msg.success){
+          console.log("success");        
+        }
+        else{
+          console.log("fail");
+          alert(msg.text);
+        }
+      },
+      error: function(data){
+        console.log("fail");
+        console.log(data);
+      }
+    })
+  })
   });
-/* 處理圖片回傳切好的50*50大小圖片*/
-document.getElementById("myimage").addEventListener('click',function(e){
-  var x = event.pageX - (this.getBoundingClientRect().x+parseInt((window.getComputedStyle(this, null).getPropertyValue('padding-left'))));
-  var y = event.pageY - (this.getBoundingClientRect().y + parseInt((window.getComputedStyle(this, null).getPropertyValue('padding-top'))));
-  if(x < 0) x = 0;
-  if(y < 0) y = 0;
-  console.log(x,y,this.getAttribute("src"));
-  $.ajax({
-    url: '/cropimage',
-    type: 'POST',
-    dataType: "json",
-    contentType: "application/json",
-    xhrFields: {
-    withCredentials: true
-    },
-    data: JSON.stringify({
-        url:this.getAttribute("src"),
-        x:parseInt(x),
-        y:parseInt(y),
-        width:this.width,
-        height:this.height
-    }),
-    success: function (data) {
-        //上傳成功之後，返回物件data         
-        console.log(data)
-        if(data.success === false){
-            alert("Crop image fail");
-        }
-        else if(data.success === true){
-            document.getElementById("target1").src = data.url;
-        }
-  }})
-}) 
+ 
+ //評價
+
+  
