3181f56deafbc4bd8a79e1878f0b0196da1d5b57
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-28 19:48:35 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -158,7 +158,7 @@ function handleMove(evt) {
     var y = evt.pageY - $('#full').position().left;
     // document.getElementById("x").innerHTML = x;
     console.log("evt.pageX= " + evt.pageX + "evt.page.Y= "+evt.pageY)
-    console.log("this.offsetLeft= " +  $('#full').offset().left + "this.offsetTop= "+$('#full').offsetTop)
+    console.log("this.offsetLeft= " +  $('#full').offset().left + "this.offsetTop= "+$('#full').offset().top)
     console.log("x= " + x + "y= "+y)
     // document.getElementById("y").innerHTML = y;
     var xpx = -x +'px'
