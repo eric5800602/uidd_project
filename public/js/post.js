8445212f10f65305fc3abf4f545882e625bbffcc
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 02:40:14 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -101,7 +101,7 @@ $.ajax({
 function slide(){
   var sc = document.getElementById("id_content").scrollTop
   var pos = $('#post_content').offset().top
-  var newpos = 679.063+pos
+  var newpos = 682.5625+pos
   console.log("sc= "+sc+"\npos= "+pos)
   console.log("outch!");
   console.log("off_left_before"+$('#post_content').offset().top)
