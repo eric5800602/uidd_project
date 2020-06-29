16fd4244f4130fd60c4ea07fbb6fc693d563e5dd
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 00:08:54 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -99,17 +99,16 @@ $.ajax({
 });
 
 $('#post_content').click(function(){
+  var viewportOffset = el.getBoundingClientRect();
+  // these are relative to the viewport, i.e. the window
+  var top = viewportOffset.top;
+  console.log("topQQ"+top)
   console.log("outch!");
   console.log("off_left_before"+$('#post_content').offset().top)
   document.getElementById("post_content").style.top=$('#post_content').offset().top+"!important"
   console.log("off_left_mid"+$('#post_content').offset().top)
-  $("#post_content").hide( "slide", 10000 , function(){
-  
+  $("#post_content").hide( "slide", 10000);
   console.log("off_left_after"+$('#post_content').offset().top)
-
-
-
-  });
   //$('#post_content').animate({"position":'absolute', "left":'-10rem'})
 })
 
