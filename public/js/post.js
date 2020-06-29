68d9dc59463fd573ac03013074f4ace16bb4a042
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-29 20:58:50 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -136,7 +136,7 @@ function like (id_click){
   }else{
     console.log('wrong')
   }
-  console.log("request_id= "+id_clicked+"\ntype= "+type+"\noper= "+oper+")
+  console.log("request_id= "+id_clicked+"\ntype= "+type+"\noper= "+oper)
   
 
 
