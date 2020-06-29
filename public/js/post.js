aa646439c2f878980de3966fa3475bdb9a912565
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-29 23:59:12 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -101,7 +101,7 @@ $.ajax({
 $('#post_content').click(function(){
   console.log("outch!");
   console.log("off_left_before"+$('#post_content').offset().top)
-  $("#post_content").style.top=$('#post_content').offset().top+"!important"
+  document.getElementById("#post_content").style.top=$('#post_content').offset().top+"!important"
   $("#post_content").hide( "slide", 1000 );
   console.log("off_left_after"+$('#post_content').offset().top)
   //$('#post_content').animate({"position":'absolute', "left":'-10rem'})
