3042e89d4af654462a2b6e0cd9185b4a303c0af9
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-29 15:47:50 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -215,8 +215,8 @@ $('#btn_submit').click(function(){
                Texture: t,
                Push: 1,
                img: cut,
-               x: x,
-               y: y,
+               x: x-40,
+               y: y-40,
              }),
              contentType: "application/json",
              success: function(data){
