ff9396c60af81bb6afead0a57ffccd8d40dbaf8a
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 03:48:16 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -102,9 +102,9 @@ function slide(){
   var sc = document.getElementById("id_content").scrollTop
   var pos = $('.content_content').offset().top
   console.log("click")
-  $("#post_content1").effect("slide",{direction: "right", mode:"hide", duration:10000})
+  $("#post_content1").effect("slide",{direction: "right", mode:"hide", duration:500})
   $('#post_content1').css({"top": pos+sc+"px"})
-  $("#post_content2").effect("slide",{direction: "left", mode:"show", duration:10000})
+  $("#post_content2").effect("slide",{direction: "left", mode:"show", duration:500})
   $('#post_content2').css({"top": pos+sc+"px"})
 }
 
