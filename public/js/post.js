4d66ce2335b2effbfa2fa2116e953e0baea5a3b1
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 20:52:40 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -143,7 +143,7 @@ function slide(thisid){
 
 
   }else{
-    console.log("!!")
+    console.log(thisid)
     $(thisid).animate({"opacity":'1'}, 500)
     $('#content_img_'+content_current).animate({"opacity":'0.5'}, 500)
   }
