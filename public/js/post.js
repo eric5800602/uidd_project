8f3a1bfd5997a776f8d0b28266cf12310581423f
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 18:47:50 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -145,21 +145,22 @@ function slide(thisid){
   }else if(content_current>num){
     // 之前的比較大，往右滑
     console.log("之前的 is BIG")
-    // $("#content_title_"+content_current).effect("slide",{direction: "right", mode:"hide", duration:500})
-    // $("#content_title_"+content_current).css({"top": pos+sc+"px"})
-    // $("#content_title_"+num).effect("slide",{direction: "left", mode:"show", duration:500})
-    // $("#content_title_"+num).css({"top": pos+sc+"px"})
+    $("#content_title_"+content_current).effect("slide",{direction: "right", mode:"hide", duration:500})
+    $("#content_title_"+content_current).css({"top": pos+sc+"px"})
+    $("#content_title_"+num).css({"top": pos+sc+"px"})
+    $("#content_title_"+num).effect("slide",{direction: "left", mode:"show", duration:500})
+    $("#content_title_"+num).css({"top": pos+sc+"px"})
   }else if(content_current<num){
     // 之前的比較小，往左滑
     console.log("現在的 is BIG")
 
 
   }
-  $("#content_title_"+content_current).effect("slide",{direction: "right", mode:"hide", duration:500})
-  $("#content_title_"+content_current).css({"top": pos+sc+"px"})
+  // $("#content_title_"+content_current).effect("slide",{direction: "right", mode:"hide", duration:500})
+  // $("#content_title_"+content_current).css({"top": pos+sc+"px"})
   // $("#content_title_"+num).css({"top": pos+sc+"px"})
-  $("#content_title_"+num).effect("slide",{direction: "left", mode:"show", duration:500})
-  $("#content_title_"+num).css({"top": pos+sc+"px"})
+  // $("#content_title_"+num).effect("slide",{direction: "left", mode:"show", duration:500})
+  // $("#content_title_"+num).css({"top": pos+sc+"px"})
   console.log("slide() click")
   // $("#post_content1").effect("slide",{direction: "right", mode:"hide", duration:500})
   // $('#post_content1').css({"top": pos+sc+"px"})
