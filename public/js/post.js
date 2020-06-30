e32c84e505da94da40abb927c0bdbcecf1f4b21a
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 21:11:53 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -160,18 +160,18 @@ function slide(thisid){
   }else if(content_current>num){
     // 之前的比較大，往右滑
     console.log("之前的 is BIG")
-    $("#content_"+content_current).effect("slide",{direction: "right", mode:"hide", duration:5000})
+    $("#content_"+content_current).effect("slide",{direction: "right", mode:"hide", duration:500})
     $("#content_"+content_current).css({"top": pos+sc+"px"})
     $("#content_"+num).css({"top": pos+sc+"px"})
-    $("#content_"+num).effect("slide",{direction: "left", mode:"show", duration:5000})
+    $("#content_"+num).effect("slide",{direction: "left", mode:"show", duration:500})
     $("#content_"+num).css({"top": pos+sc+"px"})
   }else if(content_current<num){
     // 之前的比較小，往左滑
     console.log("現在的 is BIG")
-    $("#content_"+content_current).effect("slide",{direction: "left", mode:"hide", duration:5000})
+    $("#content_"+content_current).effect("slide",{direction: "left", mode:"hide", duration:500})
     $("#content_"+content_current).css({"top": pos+sc+"px"})
     $("#content_"+num).css({"top": pos+sc+"px"})
-    $("#content_"+num).effect("slide",{direction: "right", mode:"show", duration:5000})
+    $("#content_"+num).effect("slide",{direction: "right", mode:"show", duration:500})
     $("#content_"+num).css({"top": pos+sc+"px"})
 
   }
