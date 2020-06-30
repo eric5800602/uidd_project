642da92149a4340989c7372c82e5c17c1797babc
eric5800602 <eric5800602@gmail.com>
2020-07-01 06:01:00 +0800

    [PAGES] post1.html

--- public/js/post1.js
+++ public/js/post1.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
     document.body.style.overflowY = "scroll";
   });
   document.getElementById('complete').addEventListener('click', function(){
-    if(!document.getElementById(`original${editNum}`).childNodes[0].getAttribute('src')){
+    if(document.getElementById(`original${editNum}`).childNodes[0].getAttribute('src')){
       complete = true;
       $("#addsingle").click();
     }
