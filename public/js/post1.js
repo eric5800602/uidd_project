4373e146dfee5b5348cd8d4f2e2bc5c256225b85
eric5800602 <eric5800602@gmail.com>
2020-07-01 05:02:42 +0800

    [PAGES] post1.html

--- public/js/post1.js
+++ public/js/post1.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
     axisX = x;
     axisY = y;
     var haha = $imgzoomresult.find('img').css({ top: -y, left: -x }).attr('src', $myimage.attr('src'));
-    dot.style.bottom = `${y+parseInt(window.getComputedStyle(document.getElementById("myimage")).getPropertyValue("padding-top"))/2+39}px`;
+    dot.style.top = `-${y+parseInt(window.getComputedStyle(document.getElementById("myimage")).getPropertyValue("padding-top"))+39}px`;
     dot.style.left = `${x+39}px`;
     $imgzoomresult.find('img').css('max-height','50vh');
     console.log(haha);
