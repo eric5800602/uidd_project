c644c0224fd05a44967dae74b0ac3e39c6ae13d0
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 19:13:57 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -177,11 +177,11 @@ function slide(thisid){
   }else if(content_current<num){
     // 之前的比較小，往左滑
     console.log("現在的 is BIG")
-    $("#content"+content_current).effect("slide",{direction: "left", mode:"hide", duration:500})
-    $("#content"+content_current).css({"top": pos+sc+"px"})
-    $("#content"+num).css({"top": pos+sc+"px"})
-    $("#content"+num).effect("slide",{direction: "right", mode:"show", duration:500})
-    $("#content"+num).css({"top": pos+sc+"px"})
+    $("#content_"+content_current).effect("slide",{direction: "left", mode:"hide", duration:500})
+    $("#content_"+content_current).css({"top": pos+sc+"px"})
+    $("#content_"+num).css({"top": pos+sc+"px"})
+    $("#content_"+num).effect("slide",{direction: "right", mode:"show", duration:500})
+    $("#content_"+num).css({"top": pos+sc+"px"})
 
   }
   // $("#content_title_"+content_current).effect("slide",{direction: "right", mode:"hide", duration:500})
