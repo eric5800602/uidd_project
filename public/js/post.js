e0566d21fdd0af7dab94ae6bd9736afbcc0e558f
Kazumachi124170 <andmichi1998@gmail.com>
2020-06-30 22:37:07 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -138,7 +138,7 @@ $.ajax({
 function slide(thisid){
   console.log(thisid)
   var html_modal="<p>test</p>";
-  $('#content_modal'+i).html(html_modal);
+  $(thisid).html(html_modal);
   var num = thisid.substring(12, thisid.length)
   var em = document.getElementById(thisid);
   if(window.getComputedStyle(em).getPropertyValue("opacity")=='1'){
