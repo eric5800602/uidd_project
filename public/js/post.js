799d3a7a739ae4adc1c662a7f3ae1b2255d4c9a2
Kazumachi124170 <andmichi1998@gmail.com>
2020-07-01 16:20:14 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -85,11 +85,11 @@ $.ajax({
 
       var category_html=`<p class="content_category_title">Category</p>\
                         <div class="content_category_content">\
-                        <p class="content_category_content_first content_category_content_content"  id="category_1"> ${data.post.space} </p>\
+                        <p class="content_category_content_first content_category_content_content"  id="category_1" click="loadcat"> ${data.post.space} </p>\
                         <p class="content_category_content_big">> </p>\
-                        <p class="content_category_content_second content_category_content_content" id="category_2"> ${data.post.room}</p>\
+                        <p class="content_category_content_second content_category_content_content" id="category_2" click="loadcat"> ${data.post.room}</p>\
                         <p class="content_category_content_big">></p>\
-                        <p class="content_category_content_third content_category_content_content" id="category_3"> ${data.post.pings}</p>\
+                        <p class="content_category_content_third content_category_content_content" id="category_3" click="loadcat"> ${data.post.pings}</p>\
                         `;
       $('#category').html(category_html)
       //console.log(category_html)
@@ -175,8 +175,8 @@ function mask(){
   }
 }
 
-function loadpage(){
-  console.log(loadpage);
+function loadcat(){
+  console.log("loadcat");
 
 
 }
