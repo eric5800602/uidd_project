069d3ee531df7bb66bb92b3de5da4e436432140b
Kazumachi124170 <andmichi1998@gmail.com>
2020-07-01 20:31:32 +0800

    test

--- public/js/Category.js
+++ public/js/Category.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     $('#cube').click()
     $('#two').click()
   }else if(category=="臥室"||category=="廚房"||category=="客廳"||category=="書房"||category=="浴室"||category=="套房"||category=="雅房"||category=="餐廚"||category=="玄關"||category=="戶外空間"||){
+    console.log("> <")
     $('#one').click()
     $(category).click()
   }
