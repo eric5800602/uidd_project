72c69c5b8eb77d8c6a3cfcdfd320d7e96f87d843
Kazumachi124170 <andmichi1998@gmail.com>
2020-07-01 22:21:16 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -149,7 +149,7 @@ $.ajax({
         dot_html+=`<img class="dot" src="./res/img/circle.png" style="position:absolute; top:${data.single[i].position.coordinates[1]}px; left:${data.single[i].position.coordinates[0]}px">`
       }
       for(i=0; i<data.requests.length; i++){
-        dot_html+=`<img class="dot_color" src="./res/img/circle_color.png" position:"absolute" top="" left="">`
+        dot_html+=`<img class="dot_color" src="./res/img/circle_color.png" style="position:absolute; top:${data.requests[i].position.coordinates[1]}px; left:${data.requests[i].position.coordinates[0]}px">`
       }
       $('#mask').html(dot_html)
       localStorage.setItem("first", data.post.space)
