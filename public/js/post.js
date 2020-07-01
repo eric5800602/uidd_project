eeb90b34298c3c0b1cfff184856aafc1ccf26b42
Kazumachi124170 <andmichi1998@gmail.com>
2020-07-01 14:55:25 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -174,9 +174,7 @@ function slide(thisid){
     imgsrc=src
     console.log("yeah!"+imgsrc)
     $('#modal_google').attr("src",src);
-    $('#exampleModalCenter-4').modal('toggle')
-    $('#exampleModalCenter-4').modal('focus')
-    // $('#exampleModalCenter-4').addClass('show');
+    $('#exampleModalCenter-4').addClass('show');
     // $('#exampleModalCenter-4').css({"display": "block"});
     // $('#exampleModalCenter-4').modal('show');
   }else{
