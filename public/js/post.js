c255086f3e89a0b16d2761aa4bcd0fa5b561a73a
Kazumachi124170 <andmichi1998@gmail.com>
2020-07-02 04:58:18 +0800

    test

--- public/js/post.js
+++ public/js/post.js
@@ -145,7 +145,7 @@ $.ajax({
       var dot_html=``
       for(i=0; i<data.single.length; i++){
         dot_html+=`<img class="dot" src="./res/img/circle.png" style="position:absolute; top:${data.single[i].position.coordinates[1]}px; left:${data.single[i].position.coordinates[0]}px">
-                  <div class="bubble_s bubble-bottom-left_s" style="top:${data.single[i].position.coordinates[1]}px; left:${data.single[i].position.coordinates[0]}px">${data.single[i].name}</div>`
+                  <div class="bubble_s bubble-bottom-left_s" style="top:${data.single[i].position.coordinates[1]-20}px; left:${data.single[i].position.coordinates[0]}px">${data.single[i].name}</div>`
       }
       for(i=0; i<data.requests.length; i++){
         var scale=data.requests[i].Push*0.02+1
